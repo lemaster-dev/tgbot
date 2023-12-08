8b0c12216b3eeff1f325c241e9a1413082ef7a76
lemaster-dev <130857181+lemaster-dev@users.noreply.github.com>
2023-12-08 11:54:45 +0100

    Add files via upload

---
+++ tgbot.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+import json
+
+bot = Bot('6423433010:AAHauahUuDBeG7viGWE4vKGV-6nGRFI7WGM')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('otkrit webs', web_app=WebAppInfo(url='')))
+    await message.answer('privet', reply_markup=markup)
+
+
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data)
+    await message.answer(f'Name: {res ["name"]}. Email: {res["email"]}. Phone: {res["phone"]}.')
+
+
+executor.start_polling(dp)
